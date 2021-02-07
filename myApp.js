742120ec074fc3927f5860d77c37969eda9b04a2
hwalter734 <hwalter734@users.noreply.github.com>
2021-02-07 09:38:43 -0600

    Added query function

--- myApp.js
+++ myApp.js
@@ -7,14 +7,14 @@ function newDate() {
 };
 
 app.use(function(req, res, next) {
-  console.log(req.method + " " + req.path + " - " + req.ip);
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
   next();
 });
 
 console.log('Hello World');
 
 absolutePath = __dirname + '/views/index.html'
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(absolutePath);
 });
 
@@ -22,15 +22,15 @@ app.get("/", function(req, res) {
 app.use('/public', express.static(__dirname + '/public'));
 
 // app.get('/json', function(req, res) {
-//   res.json({"message": "Hello json"})
+//   res.json({'message': 'Hello json'})
 // });
 
 app.get('/json', function(req, res) {
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        res.json({"message": "HELLO JSON"})
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({'message': 'HELLO JSON'})
     }
     else {
-        res.json({"message": "Hello json"})
+        res.json({'message': 'Hello json'})
     }
   });
 
@@ -44,6 +44,10 @@ app.get('/now', function(req, res, next) {
 
 app.get('/:word/echo', function(req, res) {
   res.json({echo: req.params.word})
+});
+
+app.get('/name', function(req, res) {
+  res.json({name: req.query.first + ' ' + req.query.last})
 });
 
  module.exports = app;
